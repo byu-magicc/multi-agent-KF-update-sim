d301a2f63de8626187e1c0a39fe194c111ffc93a
Brandon Sutherland <brandonsutherland2@gmail.com>
2025-02-11 13:55:42 -0700

    Added test code to plotter

--- src/plotters.py
+++ src/plotters.py
@@ -71,3 +71,31 @@ def plot_xy_trajectories(poses=[], covariances=[], markers=[], lines=[]):
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show() 
+
+
+if __name__ == "__main__":
+    from trajectories import sine_trajectory
+
+
+    poses = [
+        [sine_trajectory(100, np.array([[0, 0]]).T, np.array([[100, 100]]).T, 10, 5)[:2, :], "", "b"],
+        [sine_trajectory(100, np.array([[0, 0]]).T, np.array([[100, 0]]).T, 10, 5)[:2, :], "Traj", ""]
+    ]
+
+    lines = [
+        [np.array([[0, 0], [100, 100]]).T, ""],
+        [np.array([[0, 0], [100, 0]]).T, "r"]
+    ]
+
+    markers = [
+        [np.array([[50], [50]]), "g"],
+        [np.array([[50], [50]]), ""]
+    ]
+
+    covariances = [
+        [np.array([[10, 0], [10, 1]]), np.array([[20], [50]]), "b"],
+        [np.array([[12, -7], [-7, 6]]), np.array([[50], [20]]), "k"]
+    ]
+
+    plot_xy_trajectories(poses=poses, lines=lines, markers=markers, covariances=covariances)
+
