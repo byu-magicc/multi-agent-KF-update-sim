d6583df9ca5662b3b6e449f3413952751b67559d
Brandon Sutherland <brandonsutherland2@gmail.com>
2025-05-20 11:50:22 -0600

    Switched ekf back to IMU from odom

--- src/ekf.py
+++ src/ekf.py
@@ -1,73 +1,114 @@
 import numpy as np
 
 
-def _g(u_t, mu_t_1):
+def _g(u_t, mu_t_1, dt):
     """
-    Odometry propagation model. Given the previous state and the current control input,
+    IMU propagation model. Given the previous state and the current control input,
     return the predicted state.
+
+    x_t = x_t-1 + v_t-1 * dt + 0.5 * R_theta @ a_t * dt^2
+    theta_t = theta_t-1 + omega_t * dt
+    v_t = v_t-1 + R_theta @ a_t * dt
 
     Parameters:
     u_t: np.array, shape (3, 1)
-        Control input at time t.
-        [[delta_x, delta_y, delta_theta]].T
-    mu_t_1: np.array, shape (3, 1)
-        Previous state estimate.
-        [[x, y, theta]].T
+        Control input at time t. Contains the acceleration and angular velocity.
+        [[a_x, a_y, omega]].T
+    mu_t_1: np.array, shape (5, 1)
+        Previous state estimate. Contains the position, orientation, and velocity.
+        [[x, y, theta, v_x, v_y]].T
+    dt: float
+        Time step.
 
     Returns:
-    mu_t: np.array, shape (3, 1)
+    mu_t: np.array, shape (5, 1)
         Predicted state estimate.
     """
 
+    a_t = u_t[0:2]
+    omega_t = u_t[2]
+
+    p_t_1 = mu_t_1[:2]
     theta_t_1 = mu_t_1[2]
-    R = np.array([[np.cos(theta_t_1), -np.sin(theta_t_1)],
-                  [np.sin(theta_t_1), np.cos(theta_t_1)]]).squeeze()
-    xy_t = R @ u_t[:2] + mu_t_1[:2]
-    theta_t = mu_t_1[2] + u_t[2]
+    v_t_1 = mu_t_1[3:]
 
-    return np.vstack([xy_t, theta_t])
+    R_theta = np.array([[np.cos(theta_t_1), -np.sin(theta_t_1)],
+                       [np.sin(theta_t_1), np.cos(theta_t_1)]]).squeeze()
+
+    p_t = p_t_1 + v_t_1 * dt + 0.5 * R_theta @ a_t * dt**2
+    theta_t = theta_t_1 + omega_t * dt
+    v_t = v_t_1 + R_theta @ a_t * dt
+
+    return np.vstack([p_t, theta_t, v_t])
 
 
-def _G_mu(u_t, mu_t_1):
+def _G_mu(u_t, mu_t_1, dt):
     """
-    Jacobian of the odometry propagation model with respect to the state.
+    Jacobian of the IMU propagation model with respect to the state.
 
     Parameters:
     u_t: np.array, shape (3, 1)
-        Control input at time t. [[delta_x, delta_y, delta_theta]].T
-    mu_t_1: np.array, shape (3, 1)
-        State estimate at time t-1. [[x, y, theta]].T
+        Control input at time t. Contains the acceleration and angular velocity.
+        [[a_x, a_y, omega]].T
+    mu_t_1: np.array, shape (5, 1)
+        State estimate at time t-1. Contains the position, orientation, and velocity.
+        [[x, y, theta, v_x, v_y]].T
+    dt: float
+        Time step.
 
     Returns:
-    G_mu_t: np.array, shape (3, 3)
+    G_mu_t: np.array, shape (5, 5)
     """
 
-    theta_t_1 = mu_t_1.item(2)
-    G_mu_t = np.array([[1, 0, -u_t.item(0) * np.sin(theta_t_1) - u_t.item(1) * np.cos(theta_t_1)],
-                       [0, 1, u_t.item(0) * np.cos(theta_t_1) - u_t.item(1) * np.sin(theta_t_1)],
-                       [0, 0, 1]])
+    a_t = u_t[0:2]
+    theta_t_1 = mu_t_1[2]
+
+    R_prime = np.array([[-np.sin(theta_t_1), -np.cos(theta_t_1)],
+                        [np.cos(theta_t_1), -np.sin(theta_t_1)]]).squeeze()
+
+    p_prime_theta = 0.5 * R_prime @ a_t * dt**2
+    v_prime_theta = R_prime @ a_t * dt
+
+    G_mu_t = np.array([[1, 0, p_prime_theta.item(0), dt, 0],
+                       [0, 1, p_prime_theta.item(1), 0, dt],
+                       [0, 0, 1, 0, 0],
+                       [0, 0, v_prime_theta.item(0), 1, 0],
+                       [0, 0, v_prime_theta.item(1), 0, 1]])
 
     return G_mu_t
 
 
-def _G_u(u_t, mu_t_1):
+def _G_u(u_t, mu_t_1, dt):
     """
-    Jacobian of the odometry propagation model with respect to input.
+    Jacobian of the IMU propagation model with respect to input.
 
     Parameters:
     u_t: np.array, shape (3, 1)
-        Control input at time t. [[delta_x, delta_y, delta_theta]].T
-    mu_t_1: np.array, shape (3, 1)
-        State estimate at time t-1. [[x, y, theta]].T
+        Control input at time t. Contains the acceleration and angular velocity.
+        [[a_x, a_y, omega]].T
+    mu_t_1: np.array, shape (5, 1)
+        Current state estimate. Contains the position, orientation, and velocity.
+        [[x, y, theta, v_x, v_y]].T
+    dt: float
+        Time step.
 
     Returns:
-    G_u_t: np.array, shape (3, 3)
+    G_u_t: np.array, shape (5, 5)
     """
 
-    theta_t_1 = mu_t_1.item(2)
-    G_u_t = np.array([[np.cos(theta_t_1), -np.sin(theta_t_1), 0],
-                      [np.sin(theta_t_1), np.cos(theta_t_1), 0],
-                      [0, 0, 1]])
+    theta_t_1 = mu_t_1[2]
+
+    R_theta = np.array([[np.cos(theta_t_1), -np.sin(theta_t_1)],
+                      [np.sin(theta_t_1), np.cos(theta_t_1)]]).squeeze()
+
+    p_prime_a = 0.5 * R_theta * dt**2
+    v_prime_a = R_theta * dt
+
+    G_u_t = np.array([[p_prime_a[0, 0], p_prime_a[0, 1], 0],
+                      [p_prime_a[1, 0], p_prime_a[1, 1], 0],
+                      [0, 0, dt],
+                      [v_prime_a[0, 0], v_prime_a[0, 1], 0],
+                      [v_prime_a[1, 0], v_prime_a[1, 1], 0]])
 
     return G_u_t
 
@@ -77,17 +118,16 @@ def _h_global(mu_t):
     Global measurement model. Given the current state, return the predicted global pose.
 
     Parameters:
-    mu_t: np.array, shape (3, 1)
+    mu_t: np.array, shape (5, 1)
         Current state estimate. Contains the position, orientation, and velocity.
-        [[x, y, theta]].T
+        [[x, y, theta, v_x, v_y]].T
 
     Returns:
     np.array, shape (3, 1)
         Predicted global pose.
     """
 
-    return mu_t
-
+    return mu_t[:3]
 
 
 def _H_global(mu_t):
@@ -95,16 +135,16 @@ def _H_global(mu_t):
     Jacobian of the global measurement model with respect to the state.
 
     Parameters:
-    mu_t: np.array, shape (3, 1)
+    mu_t: np.array, shape (5, 1)
         Current state estimate. Contains the position, orientation, and velocity.
-        [[x, y, theta]].T
+        [[x, y, theta, v_x, v_y]].T
 
     Returns:
-    H_global_t: np.array, shape (3, 3)
+    H_global_t: np.array, shape (3, 5)
         Jacobian of the global measurement model.
     """
 
-    return np.eye(3, 3)
+    return np.eye(3, 5)
 
 
 def _h_range(mu_a_t, mu_b_t):
@@ -112,10 +152,10 @@ def _h_range(mu_a_t, mu_b_t):
     Range measurement model.
 
     Parameters:
-    mu_a_t: np.array, shape (3, 1)
-        State estimate of vehicle a. [[x_a, y_a, theta_a]].T
-    mu_b_t: np.array, shape (3, 1)
-        State estimate of vehicle b. [[x_b, y_b, theta_b]].T
+    mu_a_t: np.array, shape (5, 1)
+        State estimate of vehicle a. [[x_a, y_a, theta_a, v_x_a, v_y_a]].T
+    mu_b_t: np.array, shape (5, 1)
+        State estimate of vehicle b. [[x_b, y_b, theta_b, v_x_b, v_y_b]].T
     """
 
     return np.linalg.norm(mu_a_t[:2] - mu_b_t[:2]).item(0)
@@ -126,10 +166,10 @@ def _H_range(mu_a_t, mu_b_t):
     Jacobian of the range measurement model with respect to vehicle a's state.
 
     Parameters:
-    mu_a_t: np.array, shape (3, 1)
-        State estimate of vehicle a. [[x_a, y_a, theta_a]].T
-    mu_b_t: np.array, shape (3, 1)
-        State estimate of vehicle b. [[x_b, y_b, theta_b]].T
+    mu_a_t: np.array, shape (5, 1)
+        State estimate of vehicle a. [[x_a, y_a, theta_a, v_x_a, v_y_a]].T
+    mu_b_t: np.array, shape (5, 1)
+        State estimate of vehicle b. [[x_b, y_b, theta_b, v_x_b, v_y_b]].T
     """
 
     range = np.linalg.norm(mu_a_t[:2] - mu_b_t[:2]).item(0)
@@ -139,30 +179,35 @@ def _H_range(mu_a_t, mu_b_t):
     x_b = mu_b_t.item(0)
     y_b = mu_b_t.item(1)
 
-    return np.array([[(x_a - x_b) / range, (y_a - y_b) / range, 0]])
+    return np.array([[(x_a - x_b) / range, (y_a - y_b) / range, 0, 0, 0]])
 
 
 class EKF:
     """
-    Basic EKF class for odometry propagation and global measurements.
+    Basic EKF class for IMU propagation and global measurements.
     """
-    def __init__(self, mu_0, Sigma_0, odom_sigmas):
+    def __init__(self, mu_0, Sigma_0, imu_sigmas, dt):
         """
         Parameters:
-        mu_0: np.array, shape (3, 1)
-            Initial state estimate. Contains the position and orientation. [[x, y, theta]].T
-        Sigma_0: np.array, shape (3, 3)
+        mu_0: np.array, shape (5, 1)
+            Initial state estimate. Contains the position, orientation, and velocity
+            [[x, y, theta, v_x, v_y]].T
+        Sigma_0: np.array, shape (5, 5)
             Initial covariance matrix.
-        odom_sigmas: np.array, shape (3, 1)
-            Standard deviations of the odometry noise. [[sigma_x, sigma_y, sigma_theta]].T
+        imu_sigmas: np.array, shape (3, 1)
+            Standard deviations of the IMU noise.
+            [[sigma_acc_x, sigma_acc_y, sigma_theta_dot]].T
+        dt: float
+            Time duration of IMU measurement steps, in seconds.
         """
-        assert mu_0.shape == (3, 1)
-        assert Sigma_0.shape == (3, 3)
-        assert odom_sigmas.shape == (3, 1)
+        assert mu_0.shape == (5, 1)
+        assert Sigma_0.shape == (5, 5)
+        assert imu_sigmas.shape == (3, 1)
 
         self.mu = mu_0
         self.Sigma = Sigma_0
-        self.R = np.diag(odom_sigmas.flatten()**2)
+        self.R = np.diag(imu_sigmas.flatten()**2)
+        self.dt = dt
 
         self.g = _g
         self.h_global = _h_global
@@ -178,16 +223,16 @@ class EKF:
 
         Parameters:
         u_t: np.array, shape (3, 1)
-            Control input (odometry) at time t.
-            [[delta_x, delta_y, delta_theta]].T
+            Control input at time t. Contains the acceleration and angular velocity.
+            [[a_x, a_y, omega]].T
         """
         assert u_t.shape == (3, 1)
 
         # Calculate Jacobians
-        G_mu_t = self.G_mu(u_t, self.mu)
-        G_u_t = self.G_u(u_t, self.mu)
+        G_mu_t = self.G_mu(u_t, self.mu, self.dt)
+        G_u_t = self.G_u(u_t, self.mu, self.dt)
 
-        self.mu = self.g(u_t, self.mu)
+        self.mu = self.g(u_t, self.mu, self.dt)
         self.Sigma = G_mu_t @ self.Sigma @ G_mu_t.T + G_u_t @ self.R @ G_u_t.T
 
     def global_update(self, z_t, Q):
@@ -207,7 +252,7 @@ class EKF:
 
         K_t = self.Sigma @ H_t.T @ np.linalg.inv(H_t @ self.Sigma @ H_t.T + Q)
         self.mu = self.mu + K_t @ (z_t - self.h_global(self.mu))
-        self.Sigma = (np.eye(3) - K_t @ H_t) @ self.Sigma
+        self.Sigma = (np.eye(5) - K_t @ H_t) @ self.Sigma
 
     def shared_global_update(self, z_global, t_a_b, Sigma_global, Sigma_t):
         """
@@ -254,60 +299,64 @@ class EKF:
         Q: float
             Variance of range measurement.
         x_b: np.array, shape (3, 1)
-            Current state of other vehicle. [[x, y, theta]].T
+            Current state of other vehicle. [[x, y, theta, v_x, v_y]].T
         Sigma_b: np.array, shape (3, 3)
             Covariance of other vehicle state.
         """
-        assert x_b.shape == (3, 1)
-        assert Sigma_b.shape == (3, 3)
+        assert x_b.shape == (5, 1)
+        assert Sigma_b.shape == (5, 5)
 
         H_t = self.H_range(self.mu, x_b)
 
-        F = np.hstack((np.ones((1, 1)), -H_t))
-        Sigma = np.zeros((4, 4))
+        F = np.hstack((np.ones((1, 1)), -H_t, np.zeros((1, 2))))
+        Sigma = np.zeros((6, 6))
         Sigma[0, 0] = Sigma_z
         Sigma[1:, 1:] = Sigma_b
         Q = F @ Sigma @ F.T
 
         K_t = self.Sigma @ H_t.T @ np.linalg.inv(H_t @ self.Sigma @ H_t.T + Q)
         self.mu = self.mu + K_t * (z_t - self.h_range(self.mu, x_b))
-        self.Sigma = (np.eye(3) - K_t @ H_t) @ self.Sigma
+        self.Sigma = (np.eye(5) - K_t @ H_t) @ self.Sigma
 
 
 if __name__ == "__main__":
-    from measurements import get_odom_data
+    from measurements import get_imu_data
     from plotters import plot_overview, plot_trajectory_error, Trajectory, Covariance
     from trajectories import sine_trajectory, line_trajectory
 
-
     np.set_printoptions(linewidth=np.inf)
-    np.random.seed(0)
 
-    num_steps = 100
-    odom_sigmas = np.array([0.1, 0.1, np.deg2rad(1)]).reshape(-1, 1)
+    duration = 100
+    dt = 1.0 / 400
+    num_steps = int(duration / dt)
+    imu_sigmas = np.array([[0.1, 0.1, np.deg2rad(5.)]]).T
 
     trajectory = sine_trajectory(num_steps, np.array([[0, 0]]).T, np.array([[100, 100]]).T, 5, 2)
-    hist_indices = np.arange(0, trajectory.shape[1])
+    time_hist = np.linspace(0, dt*(trajectory.shape[1] - 1), trajectory.shape[1])
 
-    odom_data = get_odom_data(trajectory, odom_sigmas)
+    imu_data, v_b = get_imu_data(trajectory, imu_sigmas, dt)
+    theta = trajectory[2, 0]
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta), np.cos(theta)]]).squeeze()
+    v = R @ np.array([[v_b], [0]])
 
-    mu_0 = trajectory[:, 0].reshape(-1, 1).copy()
-    Sigma_0 = np.eye(3) * 1e-15
+    mu_0 = np.vstack([trajectory[:, 0].reshape(-1, 1).copy(), v])
+    Sigma_0 = np.eye(5) * 1e-15
     Sigma_global = np.diag([0.1, 0.1, 0.05])**2
 
-    mu_hist = {"Vehicle 1": [mu_0]}
+    mu_hist = {"Vehicle 1": [mu_0[:3]]}
     truth_hist = {"Vehicle 1": [trajectory]}
-    Sigma_hist = {"Vehicle 1": [Sigma_0.copy()]}
+    Sigma_hist = {"Vehicle 1": [Sigma_0.copy()[:3, :3]]}
 
-    ekf = EKF(mu_0, Sigma_0, odom_sigmas)
+    ekf = EKF(mu_0, Sigma_0, imu_sigmas, dt)
 
     for i in range(num_steps - 1):
-        ekf.propagate(odom_data[:, i].reshape(-1, 1))
+        ekf.propagate(imu_data[:, i].reshape(-1, 1))
 
-        mu_hist["Vehicle 1"].append(ekf.mu.copy())
-        Sigma_hist["Vehicle 1"].append(ekf.Sigma.copy())
+        mu_hist["Vehicle 1"].append(ekf.mu.copy()[:3])
+        Sigma_hist["Vehicle 1"].append(ekf.Sigma.copy()[:3, :3])
 
-        if i % 50 == 0 and i != 0:
+        if i*dt % 50 == 0 and i != 0:
             global_meas = trajectory[:3, i + 1].reshape(-1, 1)
             ekf.global_update(global_meas, Sigma_global)
 
@@ -318,5 +367,5 @@ if __name__ == "__main__":
                                 Trajectory(mu_hist["Vehicle 1"][0][:2], name="Estimate", color="b")],
                   covariances=[Covariance(ekf.Sigma[:2, :2], ekf.mu[:2], color="b")])
 
-    plot_trajectory_error(hist_indices, truth_hist, mu_hist, Sigma_hist)
+    plot_trajectory_error(time_hist, truth_hist, mu_hist, Sigma_hist)
 
